ea1d469858a84a2a4de1bf0c5bfb93d9ece099c8
Luca Perez <lucamarper@gmail.com>
2024-06-24 14:17:40 -0300

    Agregamos funciones y reducimos el codigo en app.js

--- logicacurso2/app.js
+++ logicacurso2/app.js
@@ -1,10 +1,25 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = "Juego del numero secreto";
+let numeroSecreto = generarNumeroSecreto();
+console.log(numeroSecreto);
 
-let parrafo = document.querySelector('p');
-parrafo.innerHTML = "Indica un numero del 1 al 10";
+function asignarTextoElemento(elemento,texto){// la funcion puede recibir parametros, dos parametros recibidos
+    let elementoHTML = document.querySelector(elemento);
+    elemento.innerHTML = texto;
+}
 
 function intentoDeUsuario(){
     alert ("click desde el boton");
+    return;
 }
+
+function generarNumeroSecreto() {
+    return math.floor (math.random()* 10) + 1;
+    return numeroSecreto;
+
+    
+}
+
+asignarTextoElemento('h1','juego del numero secreto'); // invocando a la funcion asignarTextoElemento
+asignarTextoElemento('p','Indica un numero del 1 al 10'); // invocando a la funcion asignarTextoElemento
+
+
 

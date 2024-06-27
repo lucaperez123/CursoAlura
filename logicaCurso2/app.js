3f6ab51a9ee5805d1ca72aeb3785f1e79461f232
Luca Perez <lucamarper@gmail.com>
2024-06-27 17:25:36 -0300

    Colocamos una nueva funcion para poder darle un maximo de intentos y que salga un mensaje dando dicho aviso

--- logicaCurso2/app.js
+++ logicaCurso2/app.js
@@ -1,6 +1,7 @@
 let numeroSecreto = generarNumeroSecreto();
 let intentos = 1;
 let listaNumerosSorteados = [];
+let numeroMaximo = 10;
 
 
 function asignarTextoElemento(elemento, texto) {
@@ -36,11 +37,16 @@ function limpiarCaja() {
 
 
 function generarNumeroSecreto() {
-    let numeroGenerado = Math.floor(Math.random()*10)+1;
+    let numeroGenerado = Math.floor(Math.random()*numeroMaximo)+1;
     
     console.log (numeroGenerado);
     console.log(lista);
-    
+    // si ya sorteamos todos los numeros
+    if(listaNumerosSorteados.length == numeroMaximo){
+        asignarTextoElemento('p',`Ya no hay más números para sortear`);
+    }else{
+
+
     // si el numero generado esta incluido en la lista
     if(listaNumerosSorteados.includes(numeroGenerado)){
         //se vuelve a generar un numero aleatorio
@@ -49,11 +55,12 @@ function generarNumeroSecreto() {
         listaNumerosSorteados.push(numeroGenerado);
         return numeroGenerado;
     }
+    }
 }
 
 function condicionesIniciales() {
     asignarTextoElemento('h1','Juego del número secreto!');
-    asignarTextoElemento('p',`Indica un número del 1 al 10`);
+    asignarTextoElemento('p',`Indica un número del 1 al ${numeroMaximo}`);
     numeroSecreto = generarNumeroSecreto();
     intentos = 1;
     console.log(numeroSecreto);
